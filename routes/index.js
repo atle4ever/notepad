c5975bcdc52b7d3df570c552e3c96ceed552bc6c
Kim, Seongjun <atle4ever@gmail.com>
2015-07-01 15:01:02 +0900

    implement basic function
    - add/modify notes
    - view list of notes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
-/* GET home page. */
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+/* GET list */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var files = fs.readdirSync('data');
+    res.render('list', { 'items': files  });
+});
+
+/* GET input page */
+router.get('/new', function(req, res, next) {
+    res.render('new', { });
+});
+
+/* POST save new item */
+router.post('/save_new_item', function(req, res, next) {
+    var json_str = JSON.stringify(req.body);
+    fs.writeFile(path.join('data', req.body.date), json_str);
+    logger.info("new item is saved as " + req.body.date);
+
+    res.redirect('/');
+});
+
+/* GET view item */
+router.get('/view', function(req, res, next) {
+    var date = req.query.date;
+    var json_str = fs.readFileSync(path.join('data', date));
+    var json = JSON.parse(json_str);
+    console.log(json);
+    
+    res.render('view', json);
 });
 
 module.exports = router;
